9c1fd2f48768517e36bca533b62e9044832fc368
William-Hu0 Hu <hu.william@iisgalvanimi.edu.it>
2025-04-06 22:12:08 +0000

    finito

--- pokemon.py
+++ pokemon.py
@@ -9,36 +9,83 @@ def menu():
     print(f"inserisci 0 per uscire")
 df = pd.read_csv("pokemon.csv")
 dfcomune=df[df["Rarità"]=="Comune"]
-dfcomune=df[df["Rarità"]=="Non Comune"]
-dfcomune=df[df["Rarità"]=="Rarà"]
-dfcomune=df[df["Rarità"]=="Comune"]
-numero=random.radint(1, 100)
+dfncomune=df[df["Rarità"]=="Non Comune"]
+dfrara=df[df["Rarità"]=="Rara"]
+dfuber=df[df["Rarità"]=="Ultra Rara"]
 crediti=100
-collezione={}
+collezione=pd.DataFrame()
 while True:
-    try:
-        menu()
-        d=int(input("cosa vuoi fare?"))
-        if d == 1:
+    menu()
+    d=int(input("cosa vuoi fare?"))
+    if d == 1:
+        if crediti>=10:
             for i in range(5):
-                if numero >= 70:
-                    print(dfcomune.sample())
-                elif numero >= 71 | numero <91:
-                    print(dfcomune.sample())
-                elif numero >= 91 | numero <100:
-                    print(dfcomune.sample())
-                else:
-                    print(dfcomune.sample())
+                numero=random.randint(1, 100)
+                if numero <= 70:
+                    h=dfcomune.sample()
+                    print(h)
+                    rezero = pd.DataFrame(h)
+                    collezione = pd.concat([collezione, rezero])
+                    crediti+=1
+                elif 71<= numero <91:
+                    f=dfncomune.sample()
+                    print(f)
+                    rezero = pd.DataFrame(h)
+                    collezione = pd.concat([collezione, rezero])
+                    crediti+=3
+                elif 91<= numero <100:
+                    k=dfncomune.sample()
+                    print(k)
+                    rezero = pd.DataFrame(k)
+                    colelzione = pd.concat([collezione, rezero])
+                    crediti+=6
+                elif numero==100:
+                    L=dfncomune.sample()
+                    print(L)
+                    rezero = pd.DataFrame(L)
+                    collezione = pd.concat([collezione, rezero])
+                    crediti+=888
             crediti -=10
-        elif d==2:
-            show()
-        elif d==3:
-            remove()
-        elif d==4:
-            count()
-        elif d==5:
-            svuota()
-        elif d==0:
-            break
-    except:
-        print("riprova")
+        else:
+            print("crediti insufficienti")
+    elif d==2:
+        if crediti>=100:
+            for i in range(10):
+                for i in range(5):
+                    numero=random.randint(1, 100)
+                    if numero <= 70:
+                        h=dfcomune.sample()
+                        print(h)
+                        rezero = pd.DataFrame(h)
+                        collezione = pd.concat([collezione, rezero])
+                        crediti+=1
+                    elif 71<= numero <91:
+                        f=dfncomune.sample()
+                        print(f)
+                        rezero = pd.DataFrame(f)
+                        collezione = pd.concat([collezione, rezero])
+                        crediti+=3
+                    elif 91<= numero <100:
+                        k=dfrara.sample()
+                        print(k)
+                        rezero = pd.DataFrame(k)
+                        collezione = pd.concat([collezione, rezero])
+                        crediti+=6
+                    elif numero==100:
+                        L=dfuber.sample()
+                        print(L)
+                        rezero = pd.DataFrame(L)
+                        collezione = pd.concat([collezione, rezero])
+                        crediti+=888
+                crediti -=10
+        else:
+            print("crediti insufficienti")
+    elif d==3:
+        print(collezione)
+    elif d==4:
+        print(crediti)
+    elif d==5:
+        collezione.to_csv("collezione.csv", index=False)
+        print("Collezione salvata ")
+    elif d==0:
+        break
